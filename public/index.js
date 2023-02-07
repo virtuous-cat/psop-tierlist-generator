b5077dfd770c50350699c367426a87fd36821227
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-02-07 09:46:21 -0800

    add styling

--- public/index.js
+++ public/index.js
@@ -2,26 +2,25 @@ const localData = localDataStorage("smash");
 
 if (localData.haskey("lastPokemonId")) {
   const startButton = document.querySelector(".button");
-  const buttonGroup = document.querySelector(".button-wrapper");
+  const p = document.querySelector("#button-intro");
 
-  const p = document.createElement("p");
   const resetButton = document.createElement("button");
   resetButton.innerText = "Clear Data and Start Fresh";
+  resetButton.classList.add("pass", "large");
 
   if (localData.get("lastPokemonId") < 1008) {
     p.innerText = "Looks like you've already have a smash list going!";
     startButton.innerText = "Continue Smashing";
   }
 
-  if (localData.get("lastPokemonId") === 1008) {
+  if (localData.get("lastPokemonId") >= 1008) {
     p.innerText =
-      "Looks like you've already have a tierlist built or in progress!";
-    startButton.innerText = "Skip to Tierlist";
+      "Looks like you've already have a tier list built or in progress!";
+    startButton.innerText = "Go to Tier List";
     startButton.setAttribute("href", "/tierlist.html");
   }
 
-  buttonGroup.before(p);
-  startButton.after("Or", resetButton);
+  startButton.after(resetButton);
   resetButton.addEventListener("click", () => {
     localData.clear();
     location.reload();
